47aa20f4954bd488dcdd7dbb078a2f0eeed42e10
Diego Cabre <diegocabre@gmail.com>
2024-06-26 18:51:06 -0400

    modificaciones en el html

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.get("/iniciarSesion", (req, res) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Agente Autorizado</title>
+        <link rel="stylesheet" href="style.css">
       </head>
       <body>
         <h1>Bienvenido, ${agente.email}</h1>
